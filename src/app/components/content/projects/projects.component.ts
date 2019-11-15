0cd12088f36b6152a2c900e69187b20fa88eb82c
Badji Lounès <badjilounes@MacBook-Pro-de-Badji.local>
2019-11-15 16:37:28 +0100

    blablamovie project addde

--- src/app/components/content/projects/projects.component.ts
+++ src/app/components/content/projects/projects.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   items = [
-    {poster: 'assets/img/projects/blabla-movie.png', name: 'Blabla movie'},
-    {poster: 'assets/img/projects/blabla-movie.png', name: 'Blabla movie'},
-    {poster: 'assets/img/projects/blabla-movie.png', name: 'Blabla movie'}
-  ]
+    {
+      poster: 'assets/img/projects/blabla-movie.png', 
+      name: 'Blabla movie', 
+      description: ['Angular 6', 'Material', 'Rxjs debouncer', 'Open data api'],
+      descriptionTxt: 'projects.description.blabla-movie',
+      links: {online: 'http://blablamovie.herokuapp.com', github: 'https://www.github.com/badjilounes/blabla-movie.git'}
+    }
+  ];
 
   constructor() { }
 
