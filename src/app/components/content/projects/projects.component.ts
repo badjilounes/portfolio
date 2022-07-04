cd208aa09f0c68ae7aa45d633e6ed1eaacb58d06
Lounès Badji <lounes.badji.ext@agicap.com>
2022-07-04 11:10:26 +0200

    feat: dz dialect app

--- src/app/components/content/projects/projects.component.ts
+++ src/app/components/content/projects/projects.component.ts
@@ -9,15 +9,22 @@ export class ProjectsComponent implements OnInit {
 
   items = [
     {
-      poster: 'assets/img/projects/blabla-movie.png', 
-      name: 'Blabla movie', 
+      poster: 'assets/img/projects/dz-dialect-app.png',
+      name: 'DZ Dialect App',
+      description: ['Angular 14', 'Material', 'Ngrx', 'PWA'],
+      descriptionTxt: 'projects.description.dz-dialect-app',
+      links: {online: 'https://dz-dialect-app.herokuapp.com', github: 'https://www.github.com/badjilounes/dz-dialect-app.git'}
+    },
+    {
+      poster: 'assets/img/projects/blabla-movie.png',
+      name: 'Blabla movie',
       description: ['Angular 6', 'Material', 'Rxjs debouncer', 'Open data api'],
       descriptionTxt: 'projects.description.blabla-movie',
       links: {online: 'https://blablamovie.herokuapp.com', github: 'https://www.github.com/badjilounes/blabla-movie.git'}
     },
     {
-      poster: 'assets/img/projects/prescriber.png', 
-      name: 'Prescriber', 
+      poster: 'assets/img/projects/prescriber.png',
+      name: 'Prescriber',
       description: ['Angular 8', 'Material', 'NestJS', 'Swagger', 'JWT'],
       descriptionTxt: 'projects.description.prescriber',
       links: {online: 'https://prescriber-front.herokuapp.com', github: 'https://www.github.com/badjilounes/exo1.git'}
